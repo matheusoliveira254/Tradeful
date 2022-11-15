1ff4b5c046c828be12e867317ac16d8330cd6216
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-11-15 09:44:23 -0700

    first commit develop

---
+++ Tradeful-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tradeful-App
+//
+//  Created by Matheus Oliveira on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
